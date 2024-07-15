012152588c90b3642743cb7023b31e5844221742
IndraJ1t <SINGHINDRAJIT456@GMAIL.COM>
2024-07-15 23:13:52 +0530

    firstChallenge

---
+++ 01_challenge.js
@@ -0,0 +1,20 @@
+var num = 5;
+console.log(num);
+console.log(typeof (num));
+
+let nameind = "Indrajit";
+console.log(nameind);
+console.log(typeof (nameind));
+
+nameind = "Singh"
+console.log(nameind);
+
+const ifyes = true
+console.log(ifyes);
+
+function featureRequest(value) {
+    if (typeof (value === num))
+        console.log("The character is number!");
+
+}
+featureRequest(5)
