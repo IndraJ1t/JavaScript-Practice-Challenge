d14e7f3a53dd196e987aaf4e64ec22c555e85313
IndraJ1t <SINGHINDRAJIT456@GMAIL.COM>
2024-07-17 16:26:51 +0530

    04 challenge

---
+++ 04_Challenge.js
@@ -0,0 +1,34 @@
+// While loop
+// let n = 10
+// while (n >= 1) {
+//     console.log(n);
+//     n--
+// }
+
+// let sum = 0;
+// let n = 1
+// while (n <= 10) {
+//     sum += n
+//     n++
+// }
+// console.log(sum);
+
+//Do_While loop
+
+// let n = 5
+// let facto = 1
+// do {
+//     facto *= n
+//     n--
+// }
+// while (n > 0)
+// console.log(facto);
+
+//Patern print
+
+for (i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        console.log("* ");
+    }
+    console.log("\n");
+}
