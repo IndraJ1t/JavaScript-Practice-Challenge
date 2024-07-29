d45dd6c7bf8c049f42c32e6fc02098abae81ae4b
IndraJ1t <SINGHINDRAJIT456@GMAIL.COM>
2024-07-30 00:17:55 +0530

    closures

---
+++ 15_Closures.js
@@ -0,0 +1,34 @@
+function func1() {
+    let myName = "Indrajit"
+    return new function func2() {
+        console.log(myName);
+    }
+    func2()
+}
+func1()
+
+function uniqueID() {
+    let lastID = 0;
+    return function generateNewID() {
+        lastID++;
+        return lastID;
+    }
+}
+const generation = uniqueID();
+console.log(generation());
+console.log(generation());
+console.log(generation());
+
+
+function creation() {
+    const functions = [];
+    for (let i = 0; i < 5; i++) {
+        functions.push(function () {
+            console.log(i);/* pushes a function at i position of array */
+        })
+    }
+    return functions;
+}
+const myFunc = creation();
+myFunc[0]()/* executes the function at 0 position of array */
+myFunc[4]()
