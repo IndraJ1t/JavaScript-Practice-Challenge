d7d04d3980c3b087b6d5e8b126931c4bd1de81e0
IndraJ1t <SINGHINDRAJIT456@GMAIL.COM>
2024-08-03 01:20:51 +0530

    added content

--- 16_Regular_Expressions.js
+++ 16_Regular_Expressions.js
@@ -51,3 +51,31 @@ function validation() {
     console.log(match);
 }
 validation();
+
+/*  HTML tag validation */
+
+function HTMLElements(str) {
+    const tagPattern = /<\/?([a-z]+)[^>]*>/gi;
+    const stack = [];
+    const openTags = [];
+
+    let match;
+    while ((match = tagPattern.exec(str)) !== null) {
+        const [fullMatch, tag] = match;
+        if (fullMatch.startsWith("</")) {
+            if (stack.length === 0 || stack[stack.length - 1] !== tag) {
+                // Return the first problematic opening tag
+                const errMessage = (stack.length === 0 ? openTags[0] : stack[stack.length - 1]);
+                console.log(`Error on ${errMessage} tag`);
+            }
+            stack.pop();
+        } else {
+            stack.push(tag);
+            openTags.push(tag);
+        }
+    }
+
+    console.log(stack.length === 0 ? "No Error" : openTags[0]);
+
+}
+HTMLElements("<div><b><p>hello world</p></b><div>")
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,25 @@
-reg = /javascript/g
-const text = "javascript is a fun language. javascript"
-const match = text.match(reg);
-console.lo
+function HTMLElements(str) {
+    const tagPattern = /<\/?([a-z]+)[^>]*>/gi;
+    const stack = [];
+    const openTags = [];
+
+    let match;
+    while ((match = tagPattern.exec(str)) !== null) {
+        const [fullMatch, tag] = match;
+        if (fullMatch.startsWith("</")) {
+            if (stack.length === 0 || stack[stack.length - 1] !== tag) {
+                // Return the first problematic opening tag
+                const errMessage = (stack.length === 0 ? openTags[0] : stack[stack.length - 1]);
+                console.log(`Error on ${errMessage} tag`);
+            }
+            stack.pop();
+        } else {
+            stack.push(tag);
+            openTags.push(tag);
+        }
+    }
+
+    console.log(stack.length === 0 ? "No Error" : openTags[0]);
+
+}
+HTMLElements("<div><b><p>hello world</p></b><div>")
